3024a8b813d0c1029bbf4fb0a4650f2321110546
Henrik Vedal <vedalhenrik@gmail.com>
2018-10-02 13:12:37 +0200

    commit changes
    commitchanges

--- Server/server.js
+++ Server/server.js
@@ -5,13 +5,9 @@ const port = (process.env.PORT || 8080);
 
 const users = [];
 
-<<<<<<< HEAD
-//let output = document.getElementById("output");
-=======
 
 
 
->>>>>>> 6cc7900f2ecd7eaf3d047903cac431a965a209e4
 
 
 ourServer.set('port', port);
@@ -24,29 +20,13 @@ ourServer.listen(ourServer.get('port'), function () {
 
 ourServer.post("/app/user", function (req, res) {
     let user = req.body;
-<<<<<<< HEAD
-    
-    user.id = users.length + 1;
-    users.push(user);
-    res.json(user).end();
-    console.log(req.body);
-    
-    
-    
-    
-    
-=======
     user.id = users.length + 1;
     users.push(user);
     
     res.json(user).end();
     console.log(req.body);
     
->>>>>>> 6cc7900f2ecd7eaf3d047903cac431a965a209e4
 });
-
-
-
 
 ourServer.post("/app/login", function (req, res) {
    
@@ -55,8 +35,8 @@ ourServer.post("/app/login", function (req, res) {
     for(let i in users){
         if(login.name == users[i].name && login.password == users[i].password){
                let username = users[i].name;
-                console.log(username);
-            res.send("ok");
+                //console.log(username);
+                res.send("ok");
                 //Sender ut brukernavn
                 ourServer.get('/app/login', function(req,res, next){
                 res.json(username).end();
