d531ea08797143e7933b7e4d2db2f621618055a4
Magnus Nordbø <nordbom@live.no>
2018-10-22 13:09:33 +0200

    Oppdaterte packages

--- js/users.js
+++ js/users.js
@@ -5,14 +5,12 @@ const db = require('./dbconnect').db; //database
 const bcrypt = require('bcryptjs');
 
 
-
-
 router.post("/login/", async function (req, res) {
     let username = req.body.username;
     let password = req.body.password;
     let query = `SELECT *
 	FROM public."users" WHERE username = '${username}'`;
-    
+
     try {
         let datarows = await db.any(query);
         console.log(datarows);
@@ -44,9 +42,6 @@ router.post("/login/", async function (req, res) {
 });
 
 
-
-
-
 router.post("/register/", async function (req, res) {
     let userEmail = req.body.email;
     let userName = req.body.username;
@@ -54,10 +49,7 @@ router.post("/register/", async function (req, res) {
     let hashPassw = bcrypt.hashSync(password, 10);
 
     let query = `INSERT INTO public."users" ("id", "email", "username", "hash") VALUES (DEFAULT, '${userEmail}', '${userName}', '${hashPassw}') RETURNING "id", "email", "username", "hash"`;
-    
-    
 
-    
 
     try {
         let code = db.any(query) ? 200 : 500;
@@ -70,8 +62,6 @@ router.post("/register/", async function (req, res) {
     }
 
 });
-
-
 
 
 router.post("/changePass/", async function (req, res) {
@@ -131,7 +121,6 @@ router.post("/changePass/", async function (req, res) {
 
 
 });
-
 
 
 //export module -------------------------------------
